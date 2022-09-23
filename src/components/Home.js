447214fd3b77d94d207cd6c6a6c112eb0072938e
John Okwaro <john.okwaro@student.moringaschool.com>
2022-09-23 17:00:41 +0300

    Completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const h1Style = "firebricks"
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return 
+  <div id="home">
+    <h1 style={h1Style}>{name} is a Web developer from {city}</h1>
+  </div>;
+  
 }
 
 export default Home;
